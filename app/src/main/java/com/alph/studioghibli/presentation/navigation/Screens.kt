063178ee9facd1c87f8e37115ac2c63c7b894c5b
lvinist <alvin.geomatics@gmail.com>
2022-07-18 10:56:46 +0700

    Implemented Bubble Bottom Bar and Material Theme 3

--- app/src/main/java/com/alph/studioghibli/presentation/navigation/Screens.kt
+++ app/src/main/java/com/alph/studioghibli/presentation/navigation/Screens.kt
@@ -4,7 +4,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Screens(
